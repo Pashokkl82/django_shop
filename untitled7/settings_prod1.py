910bcb0784dcdcdbf502198ca8eb65e58e205994
Pavel Kluchnikov <pashokkl@mail.ru>
2020-03-10 15:42:44 +0300

    Settings

--- untitled7/settings_prod1.py
+++ untitled7/settings_prod1.py
@@ -5,8 +5,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER': 'django_shop',
-        'PASSWORD': '12345',
+        'USER': 'ad',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '',
     }
